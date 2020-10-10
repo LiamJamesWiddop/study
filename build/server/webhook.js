fefdc61fb19172a40fb0a52c217092e6ff11acdc
RoundhouseMouse <liamjameswiddop@gmail.com>
2020-10-11 03:45:29 +1030

    webhook multiple attempt

--- build/server/webhook.js
+++ build/server/webhook.js
@@ -4,19 +4,16 @@ const api_1 = require("./api");
 const { dialogflow, Image, } = require('actions-on-google');
 const dialogApp = dialogflow();
 dialogApp.intent('Quiz', async (conv) => {
-    console.log(conv);
+    console.log(conv.parameters[`quiz-topic`]);
     let question = await api_1.default.getBest(null, 0);
-    conv.ask(question[0].body.question);
+    console.log(question);
+    conv.followup('quiz-question', {
+        question: question[0]
+    });
 });
-dialogApp.intent('AskQuestion', async (conv) => {
+dialogApp.intent('Quiz_Question_Answer', conv => {
     console.log(conv);
     conv.close(`Ooh Ahh Glen McGra`);
-});
-dialogApp.intent('Goodbye', conv => {
-    conv.close('See you later!');
-});
-dialogApp.intent('Default Fallback Intent', conv => {
-    conv.ask(`I didn't understand. Can you tell me something else?`);
 });
 exports.default = dialogApp;
 //# sourceMappingURL=webhook.js.map
--- server/webhook.ts
+++ server/webhook.ts
@@ -9,24 +9,19 @@ const dialogApp = dialogflow();
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
 dialogApp.intent('Quiz', async (conv) => {
-    console.log(conv);
+    console.log(conv.parameters[`quiz-topic`]);
     let question = await API.getBest(null,0)
-    conv.ask(question[0].body.question);
-    // Respond with the user's lucky number and end the conversation.
-});
-dialogApp.intent('AskQuestion', async (conv) => {
-    console.log(conv);
-    conv.close(`Ooh Ahh Glen McGra`)
+    console.log(question);
+    conv.followup('quiz-question', {
+        question:question[0]
+    });
     // Respond with the user's lucky number and end the conversation.
 });
 
 // Intent in Dialogflow called `Goodbye`
-dialogApp.intent('Goodbye', conv => {
-    conv.close('See you later!')
-})
-
-dialogApp.intent('Default Fallback Intent', conv => {
-    conv.ask(`I didn't understand. Can you tell me something else?`)
+dialogApp.intent('Quiz_Question_Answer', conv => {
+    console.log(conv);
+    conv.close(`Ooh Ahh Glen McGra`)
 })
 
 export default dialogApp;
