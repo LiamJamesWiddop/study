c85e9fc13db727554205fe86455010534101f0f3
RoundhouseMouse <liamjameswiddop@gmail.com>
2020-10-11 02:05:33 +1030

    removed default intent

--- build/server/webhook.js
+++ build/server/webhook.js
@@ -6,14 +6,6 @@ dialogApp.intent('Quiz', (conv, { topic }) => {
     console.log(topic);
     conv.close('Your lucky number is 7!');
 });
-dialogApp.intent('Default Welcome Intent', conv => {
-    conv.ask('Hi, how is it going?');
-    conv.ask(`Here's a picture of a cat`);
-    conv.ask(new Image({
-        url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-        alt: 'A cat',
-    }));
-});
 dialogApp.intent('Goodbye', conv => {
     conv.close('See you later!');
 });
--- server/webhook.ts
+++ server/webhook.ts
@@ -12,16 +12,6 @@ dialogApp.intent('Quiz', (conv, {topic}) => {
     conv.close('Your lucky number is 7!');
 });
 
-
-dialogApp.intent('Default Welcome Intent', conv => {
-    conv.ask('Hi, how is it going?')
-    conv.ask(`Here's a picture of a cat`)
-    conv.ask(new Image({
-        url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-        alt: 'A cat',
-    }))
-})
-
 // Intent in Dialogflow called `Goodbye`
 dialogApp.intent('Goodbye', conv => {
     conv.close('See you later!')
