0e3bdbf374702e4a0e054f629c9b7d1632ff4c98
RoundhouseMouse <liamjameswiddop@gmail.com>
2020-10-11 21:32:30 +1030

    check conditions

--- server/webhook.ts
+++ server/webhook.ts
@@ -24,6 +24,8 @@ let newQuestion = async conv =>{
 dialogApp.intent('Quiz_Question_Next', newQuestion);
 dialogApp.intent('Quiz_Another',conv=>{
     let next = conv.parameters[`next`][0];
+    console.log(conv.parameters[`next`],next,next=='true');
+    
     if(next == 'true'){
         conv.followup('quiz-question-next', {});
     }else{
@@ -58,6 +60,7 @@ dialogApp.intent('Quiz_Answer', conv => {
 dialogApp.intent('Quiz_Answer_Followup', conv => {
     conv.data.lastAnswer = 'correct';
     let correct = conv.parameters[`correct`][0];
+    console.log(conv.parameters[`correct`],correct,correct=='true');
     let followup = 'quiz-answer-correct';
     if(correct == 'true') {
         followup = 'quiz-answer-incorrect';
